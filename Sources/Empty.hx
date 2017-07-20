bd1d8075f5e7b8d1fa2c3c58cfe7d8f7392cce29
Marian Thull <marian.thull@gmail.com>
2017-07-20 15:57:40 +0200

    test project to experiment with android audio

--- Sources/Empty.hx
+++ Sources/Empty.hx
@@ -3,17 +3,46 @@ package;
 import kha.Framebuffer;
 import kha.Scheduler;
 import kha.System;
+import kha.Assets;
+import kha.audio2.Audio1;
+import kha.audio2.AudioChannel;
+import kha.math.FastMatrix3;
+import kha.Image;
+import kha.Scaler;
 
 class Empty {
+	public static inline var width = 1920;
+	public static inline var height = 1080;
+	private var backbuffer: Image;
+	private var musicChannel: AudioChannel;
+	
 	public function new() {
-		System.notifyOnRender(render);
-		Scheduler.addTimeTask(update, 0, 1 / 60);
+		Assets.loadEverything(function () {
+			backbuffer = Image.createRenderTarget(width, height);
+			Audio1.play(Assets.sounds.bills);
+
+			System.notifyOnRender(render);
+			Scheduler.addTimeTask(update, 0, 1 / 60);
+		});
 	}
 
 	function update(): Void {
 		
 	}
 
-	function render(framebuffer: Framebuffer): Void {		
+	function render(framebuffer: Framebuffer): Void {
+		var g = backbuffer.g2;
+		
+		g.begin();
+		g.clear(kha.Color.Black);
+		g.imageScaleQuality = kha.graphics2.ImageScaleQuality.High;
+
+		// test android project
+		g.drawImage(Assets.images.testbild, 0, 0);
+
+		g.end();
+		framebuffer.g2.begin();
+		Scaler.scale(backbuffer, framebuffer, System.screenRotation);
+		framebuffer.g2.end();
 	}
 }
--- khafile.js
+++ khafile.js
@@ -1,5 +1,6 @@
 let project = new Project('Empty');
 
+project.addAssets('Assets/**');
 project.addSources('Sources');
 
 resolve(project);
