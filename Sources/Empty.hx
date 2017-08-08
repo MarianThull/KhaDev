4d529b15f058f3c6399c27ab55e3121afb97c624
Marian Thull <marian.thull@gmail.com>
2017-08-08 16:57:28 +0200

    testing audio1.Audio backend

--- Sources/Empty.hx
+++ Sources/Empty.hx
@@ -2,10 +2,12 @@ package;
 
 import kha.Framebuffer;
 import kha.Scheduler;
+import kha.Sound;
 import kha.System;
 import kha.Assets;
-import kha.audio2.Audio1;
+import kha.audio1.Audio;
 import kha.audio1.AudioChannel;
+import kha.graphics2.Graphics;
 import kha.math.FastMatrix3;
 import kha.Image;
 import kha.Scaler;
@@ -14,7 +16,11 @@ class Empty {
 	public static inline var width = 1920;
 	public static inline var height = 1080;
 	private var backbuffer: Image;
-	private var musicChannel: AudioChannel;
+	private var g: Graphics;
+	private var ac: AudioChannel;
+	private var startTime: Float;
+	private var time: Float;
+	private var paused = false;
 	
 	public function new() {
 		Assets.loadEverything(function () {
@@ -22,19 +28,39 @@ class Empty {
 		});
 		
 		backbuffer = Image.createRenderTarget(width, height);
+		g = backbuffer.g2;
 
 		System.notifyOnRender(render);
 		Scheduler.addTimeTask(update, 0, 1 / 60);
-		musicChannel = Audio1.play(Assets.sounds.pigeon);
+		ac = createAudioChannel(Assets.sounds.bills, false);
+		startTime = Scheduler.realTime();
+	}
+	
+	private function createAudioChannel(s: Sound, loop: Bool): AudioChannel {
+		var ac = Audio.play(s, loop);
+		if (ac == null) {
+			ac = Audio.stream(s, loop);
+		}
+		return ac;
 	}
 
 	function update(): Void {
-		
+		time = Scheduler.realTime() - startTime;
+		if (time > 1 && time < 2 && !paused) {
+			ac.pause();
+			paused = true;
+		}
+		else if (time > 2 && paused) {
+			ac.volume = 0.5;
+			ac.play();
+			paused = false;
+		}
+		else if (time > 6) {
+			trace('Time: ${ac.position}/${ac.length}\tfinished: ${ac.finished}');
+		}
 	}
 
-	function render(framebuffer: Framebuffer): Void {
-		var g = backbuffer.g2;
-		
+	function render(framebuffer: Framebuffer): Void {		
 		g.begin();
 		g.clear(kha.Color.Black);
 		g.imageScaleQuality = kha.graphics2.ImageScaleQuality.High;
