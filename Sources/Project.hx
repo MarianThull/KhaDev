ddc7673b892bb1b99ee783209cd5397edf9401af
Marian Thull <marian.thull@gmail.com>
2017-08-15 16:25:43 +0200

    test  Assets.loadImageFromPath method on android-native

--- Sources/Project.hx
+++ Sources/Project.hx
@@ -1,20 +1,49 @@
 package;
 
+import kha.Assets;
 import kha.Framebuffer;
+import kha.Image;
 import kha.Scheduler;
 import kha.System;
+import kha.Scaler;
 
 class Project {
+	private var image: Image;
+	public static inline var width = 1920;
+	public static inline var height = 1080;
+	private var backbuffer: Image;
+
 	public function new() {
+		 backbuffer = Image.createRenderTarget(width, height);
+		
+		 Assets.loadImageFromPath('wall.k', true, function(i: Image) {
+		 	image = i;
+		 });
+		// trace('width: ${image.width}, height: ${image.height}');
+		
+		//Assets.loadImage(Assets.images.wallName ,function(i: kha.Image) {
+			//this.image = i;
+		//});
+		
+		
+
+
 		System.notifyOnRender(render);
 		Scheduler.addTimeTask(update, 0, 1 / 60);
 	}
 
 	function update(): Void {
-		
+
 	}
 
 	function render(framebuffer: Framebuffer): Void {
-		
+		var g = backbuffer.g2;
+		g.begin();
+		g.drawImage(image, 0, 0);
+		g.end();
+
+		framebuffer.g2.begin();
+		Scaler.scale(backbuffer, framebuffer, System.screenRotation);
+		framebuffer.g2.end();
 	}
 }
--- korefile.js
+++ korefile.js
@@ -2,8 +2,8 @@ let fs = require('fs');
 let path = require('path');
 let project = new Project('AndrNatDebug', __dirname);
 project.targetOptions = {"html5":{},"flash":{},"android":{},"ios":{}};
-project.setDebugDir('build/android-native');
-Promise.all([Project.createProject('build/android-native-build', __dirname), Project.createProject('C:/Users/Dev/KOM/KhaDev/kha', __dirname), Project.createProject('C:/Users/Dev/KOM/KhaDev/kha/Kore', __dirname)]).then((projects) => {
+project.setDebugDir('build/html');
+Promise.all([Project.createProject('build/html-build', __dirname), Project.createProject('C:/Users/Dev/KOM/KhaDev/kha', __dirname), Project.createProject('C:/Users/Dev/KOM/KhaDev/kha/Kore', __dirname)]).then((projects) => {
 	for (let p of projects) project.addSubProject(p);
 	let libs = [];
 	Promise.all(libs).then((libprojects) => {
