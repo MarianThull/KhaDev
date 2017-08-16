fc6cbe2fbc49c31a056d3b434e6d1c07880af4ba
Marian Thull <marian.thull@gmail.com>
2017-08-16 23:40:28 +0200

    test project for issue #453

--- Sources/Project.hx
+++ Sources/Project.hx
@@ -8,38 +8,64 @@ import kha.System;
 import kha.Scaler;
 
 class Project {
-	private var image: Image;
-	public static inline var width = 1920;
-	public static inline var height = 1080;
+	public static inline var width = 1024;
+	public static inline var height = 768;
 	private var backbuffer: Image;
 
+	private var globScale: Float = 1;
+	private var lineX0 = 0;
+	private var lineY0 = 0;
+	private var lineX1 = width/2;
+	private var lineY1 = height/2;
+	private var posX:Float = 0;
+	private var posY:Float = 0;
+	private var hold = false;
+
 	public function new() {
-		 backbuffer = Image.createRenderTarget(width, height);
-		
-		 Assets.loadImageFromPath('wall.k', true, function(i: Image) {
-		 	image = i;
-		 });
-		// trace('width: ${image.width}, height: ${image.height}');
-		
-		//Assets.loadImage(Assets.images.wallName ,function(i: kha.Image) {
-			//this.image = i;
-		//});
-		
-		
+		backbuffer = Image.createRenderTarget(width, height);
+		Assets.loadEverything(function() {});
 
-
+		kha.input.Mouse.get().notify(mouseDown, mouseUp, mouseMove, null);
 		System.notifyOnRender(render);
 		Scheduler.addTimeTask(update, 0, 1 / 60);
 	}
 
-	function update(): Void {
+	private function mouseDown(button: Int, x: Int, y: Int): Void {
+		hold = true;
+	}
 
+	private function mouseUp(button: Int, x: Int, y: Int): Void {
+		hold = false;
+	}
+
+	private function mouseMove(x:Float, y:Float, dx:Float, dy:Float): Void {
+		if (hold) {
+			lineX1 += dx * globScale;
+			lineY1 += dy * globScale;
+		}
+		posX = x;
+		posY = y;
+	}
+
+	private function updateGlobScale(): Void {
+		
+	}
+
+	function update(): Void {
+		updateGlobScale();
 	}
 
 	function render(framebuffer: Framebuffer): Void {
 		var g = backbuffer.g2;
+		// g.font = Assets.fonts.arial;
+		// g.fontSize = 12;
 		g.begin();
-		g.drawImage(image, 0, 0);
+		g.color = kha.Color.Orange;
+		g.drawRect(0, 0, width, height, 4);
+		g.drawLine(lineX0, lineY0, lineX1, lineY1, 4);
+		// g.drawString('(${posX}, ${posY})', 0, 0);
+		trace('(${posX}, ${posY})');
+		g.color = kha.Color.Black;
 		g.end();
 
 		framebuffer.g2.begin();
--- khafile.js
+++ khafile.js
@@ -1,4 +1,6 @@
 let project = new Project('AndrNatDebug');
 project.addAssets('Assets/**');
 project.addSources('Sources');
+project.windowOptions.width = 1024;
+project.windowOptions.height = 768;
 resolve(project);
